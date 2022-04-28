ad3bb169b6d192956196302fbd990f91b28a2be9
paudenn <pau2001r.b@gmail.com>
2022-04-28 13:44:10 +0200

    EntregaMinim1FINAL

--- EA-Seminar6-Client/src/app/app-routing.module.ts
+++ EA-Seminar6-Client/src/app/app-routing.module.ts
@@ -12,11 +12,12 @@ const routes: Routes = [
   { path: '', component: ListarUsersComponent},
   { path: 'crear-user', component: CrearUserComponent},
   { path: 'editar-user/:name', component: CrearUserComponent},
-  { path: '**', redirectTo: '', pathMatch: 'full'},// In case of a wrong URL, the code redirects to the main path
 
   { path: 'lista-reports', component: ListarReportsComponent},
   { path: 'crear-report', component: CrearReportComponent},
   { path: 'editar-report/:id', component: CrearReportComponent},
+  { path: '**', redirectTo: '', pathMatch: 'full'},// In case of a wrong URL, the code redirects to the main path
+
 
 
 ];
